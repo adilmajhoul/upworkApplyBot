49ac581641b37c66972ec7dc639741f5c4f1d825
Adil_Majhoul <itsadilhadid@gmail.com>
2024-05-13 00:06:38 +0100

    is the last commit deletes ?

---
+++ instagram_puppeteer_bot/main.js
@@ -0,0 +1,13 @@
+const InstagramBot = require('./InstagramBot');
+
+async function main() {
+  const instagramBot = new InstagramBot();
+
+  await instagramBot.init();
+  await instagramBot.signin({
+    username: process.env.username,
+    password: process.env.password,
+  });
+}
+
+main().then(console.error);
